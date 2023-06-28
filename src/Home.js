de9ed90c33fb9c2b252b6211c8c4e52fd285cb66
Kamlesh kumar <kamleshkumardbg22@gmail.com>
2023-06-28 13:30:58 +0530

    create-incre-dec

---
+++ src/Home.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+
+const Home = () => {
+
+
+    const dispatch = useDispatch();
+    const { c } = useSelector(state => state.custom);
+
+    const addBtn = () => {
+        dispatch({
+            type: "increment",
+        })
+
+    }
+    const addBtn25 = () => {
+        dispatch({
+            type: "incrementByValue",
+            payload: 25,
+        })
+
+    }
+
+    const subBtn = () => {
+
+        dispatch({
+            type: "decrement",
+        })
+    }
+
+    return (
+        <div>
+            <h2>{c}</h2>
+
+
+            <button onClick={addBtn}>Increment</button>
+            <button onClick={addBtn25}>Increment by 25</button>
+            <button onClick={subBtn}>Decrement</button>
+        </div>
+    )
+}
+
+export default Home
